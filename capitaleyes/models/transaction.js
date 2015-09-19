5d30e3ef7ff75eabf65e6877568fbecaa6b815f1
Heeyoon <heeyoon@mit.edu>
2015-09-19 15:44:24 -0400

    Adding models

---
+++ capitaleyes/models/transaction.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+ObjectId = Schema.ObjectId;
+var transactionSchema = new Schema({
+	transaction_id: String,
+	description: String,
+	amount: Number,
+	date: Date,
+	tag: String
+});
+
+var Transaction = mongoose.model('Transaction', transactionSchema);
+
+
+var checkLength = function(s) {
+  return s.length > 0;
+};
+
+Transaction.schema.path('description').validate(checkLength, "Description cannot be empty");
+Transaction.schema.path('amount').validate(checkLength, "Amount of money cannot be empty");
+Transaction.schema.path('date').validate(checkLength, "Date cannot be empty");
+exports.Transaction = Transaction;
---
+++ capitaleyes/models/users.js
@@ -0,0 +1,24 @@
+//User Schema page: defines user object and associated information
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+//Mongoose schema for users
+//currently only password and username
+var usersSchema = new mongoose.Schema({
+	username: String,
+	password: String
+});
+
+//Associated method for usersSchema:
+//generating a hash for the person
+usersSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+
+//checking if a password is valid
+usersSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+//create entire model to expose it to the entire application
+var User = mongoose.model('User', usersSchema);
+exports.User = User;
