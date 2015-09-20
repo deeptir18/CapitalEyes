7f7a67e349791240168706f58fcc89a985175e86
kiranw <kiranw@mit.edu>
2015-09-20 04:10:42 -0400

    yas

--- public/js/load_page.js
+++ public/js/load_page.js
@@ -1,13 +1,14 @@
 var transactions;
+var events = [];
 
 var login = function() {
 
 	var userID = "55e94a6cf8d8770528e618dc";
 	html = '<table class="table">' + '<thead id="theadspecial">' + 
-		        '<td class="name">Transaction Name</td>' + 
-		        '<td class="date">Date</td>' + 
-		        '<td class="amount">Amount</td>' + 
-		        '<td class="tags">Tags</td>' + 
+		        '<td class="pls name">Transaction Name</td>' + 
+		        '<td class="pls date">Date</td>' + 
+		        '<td class="pls amount">Amount</td>' + 
+		        '<td class="pls tags">Tags</td>' + 
 		        // '<td class="editTag">Edit Tag</td>' + 
 		    '</thead>';
 	transactions = [];
@@ -19,15 +20,14 @@ var login = function() {
 			for (var i=0; i<data.length; i++)
 			{
 				var temp_html = "<tr>";
-				temp_html += "<td id='" + data[i]._id + "' class='name'>" + data[i].description + "</td>"
-				temp_html += "<td id='" + data[i]._id + "' class='date'>" + data[i].purchase_date + "</td>"
+				temp_html += "<td id='" + data[i]._id + "' class='pls name'>" + data[i].description + "</td>"
+				temp_html += "<td id='" + data[i]._id + "' class='pls date'>" + data[i].purchase_date + "</td>"
 				balance -= data[i].amount;
-				temp_html += "<td id='" + data[i]._id + "' class='amount negative'>" + "-"+data[i].amount + "</td>"
-				temp_html += "<td id='" + data[i]._id + "' class='tags'>" + '<div class="tag">' + tags[data[i]._id] + '</div>' + "</td>"
-				// temp_html += "<td id='" + data[i]._id + "' class='editTag'>" + "+" + "</td>"
+				temp_html += "<td id='" + data[i]._id + "' class='pls amount negative'>" + "-$"+data[i].amount + "</td>"
+				temp_html += "<td id='" + data[i]._id + "' class='pls tags'>" + '<div class="tag">' + tags[data[i]._id] + '</div>' + "</td>"
 				temp_html += "</tr>";
 				transactions.push({"date":data[i].purchase_date, "html":temp_html})
-				// html += temp_html;
+				events.push({"title":data[i].description + ", " + "-$"+data[i].amount, "start":new Date(data[i].purchase_date)})
 			}
 		});
 
@@ -38,21 +38,18 @@ var login = function() {
 			for (var i=0; i<data.length; i++)
 			{
 				var temp_html = "<tr>";
-				temp_html += "<td id='" + data[i]._id + "' class='name'>" + data[i].description + "</td>"
-				temp_html += "<td id='" + data[i]._id + "' class='date'>" + data[i].transaction_date + "</td>"
-				temp_html += "<td id='" + data[i]._id + "' class='amount positive'>" + data[i].amount + "</td>"	
+				temp_html += "<td id='" + data[i]._id + "' class='pls name'>" + data[i].description + "</td>"
+				temp_html += "<td id='" + data[i]._id + "' class='pls date'>" + data[i].transaction_date + "</td>"
+				temp_html += "<td id='" + data[i]._id + "' class='pls amount positive'>$" + data[i].amount + "</td>"	
 				balance += data[i].amount;
-				temp_html += "<td id='" + data[i]._id + "' class='tags'>" + '<div class="tag">' + tags[data[i]._id] + '</div>' + "</td>"
-				// temp_html += "<td id='" + data[i]._id + "' class='editTag'>" + "+" + "</td>"
+				temp_html += "<td id='" + data[i]._id + "' class='pls tags'>" + '<div class="tag">' + "Income" + '</div>' + "</td>"
 				temp_html += "</tr>";
-				// html += temp_html;
 				transactions.push({"date":data[i].transaction_date, "html":temp_html})
+				events.push({"title":data[i].description + ", " + "-$"+data[i].amount, "start":new Date(data[i].purchase_date)})
 			}
-			// console.log(transactions)
 			transactions.sort(function(a,b){
   				return new Date(b.date) - new Date(a.date);
 			});
-			console.log(transactions)
 
 			for (var i=0; i<transactions.length; i++) {
 				html += transactions[i].html;
@@ -60,22 +57,31 @@ var login = function() {
 
 			html += "</table>";
 			if (balance > 0) {
-				balhtml = "<span class='positive'>$" + balance + "</span>"	
+				balhtml = "<span class='positive'>$" + Math.round(balance * 100) / 100 + "</span>"	
 			} else {
-				balhtml = "<span class='negative'>" + balance + "</span>"	
+				balhtml = "<span class='negative'>" + Math.round(balance * 100) / 100 + "</span>"	
 			}
 			
 			$("#transactions").append(html);
 			$(".balanceval").html(balhtml);
+
+			$('#calendar').fullCalendar({
+	        // put your options and callbacks here
+		        events: events,
+		        allDayDefault: true
+			})
 		});
 }
 
-$(document).ready(function() {
-	$('#calendar').fullCalendar({
-	        // put your options and callbacks here
-	})
+// $(document).ready(function() {
+// 	$('#calendar').fullCalendar({
+// 	        // put your options and callbacks here
+// 	        events: events,
+// 	        color: "yellow",
+// 	        textColor: "black"
+// 	})
 	
-});
+// });
 
 
 var tags = {"55fe05f9ce1cef140015e34f":"Food", "55fe064dce1cef140015e350":"Food", "55fe0679ce1cef140015e351":"Food", "55fe0687ce1cef140015e352":"Food", "55fe068bce1cef140015e353":"Food", "55fe06d4ce1cef140015e354":"Entertainment", "55fe08d8ce1cef140015e358":"Utilities", "55fe38dace1cef140015e3b4":"Income", "55fe3965ce1cef140015e3b5":"Income", "55fe3997ce1cef140015e3b6":"Income"}
