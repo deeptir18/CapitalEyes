9be8104d4b7ce16f43b94ff0b0489a208aa012a7
kiranw <kiranw@mit.edu>
2015-09-19 22:48:17 -0400

    updates

--- capitaleyes/public/js/ajax.js
+++ capitaleyes/public/js/ajax.js
@@ -1,38 +1,40 @@
-$.ajax({
-	type: "POST",
-	url: "/addTransaction",
-	data: "&transactionID=" + transactionID + "&userID=" + userID + "&tag=" + tag,
-	success: function(data) {
-		transactionID = data.transactionID
-		userID = data.userID
-		tag = data.tag
-
-		//change stuff in the UI
-
-	}
-
-
-});
+// function updateTag() {
+// 	$.ajax({
+// 		type: "POST",
+// 		url: "/addTransaction",
+// 		data: "&transactionID=" + transactionID + "&userID=" + userID + "&tag=" + tag,
+// 		success: function(data) {
+// 			console.log(data)
+// 			// transactionID = data.transactionID
+// 			// userID = data.userID
+// 			// tag = data.tag
+// 		}
 
 
-$.ajax({
-	type: "POST",
-	url: "/updateTransaction",
-	data: "&transactionID=" + transactionID + "&tag=" + tag,
-	success: function(data) {
-		transactionID = data.transactionID
-		tag = data.tag
+// 	});	
+// }
 
-		//change stuff in the UI
-	}
-	
+//multiple tags
+function addMoreTags() {
+	$.ajax({
+		type: "POST",
+		url: "/updateTransaction",
+		data: "&transactionID=" + transactionID + "&tag=" + tag,
+		success: function(data) {
+			transactionID = data.transactionID
+			tag = data.tag
+		}
+		
 
-});
+	});
+}
 
+var userID = "55e94a6cf8d8770528e618dc";
 
 $.ajax({
 	type: "GET",
 	url: "/login",
+	data: "&userID=" + userID
 	success: function(data) {
 		transactionID = data.transactionID
 		tag = data.tag
--- capitaleyes/public/js/load_page.js
+++ capitaleyes/public/js/load_page.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
 	$('#calendar').fullCalendar({
 	        // put your options and callbacks here
 	})
+	
 });
+
+var test = function() {
+	$.ajax({
+		    type: "POST",
+		    url: "http://api.reimaginebanking.com/accounts/" + "55e94a6cf8d8770528e6190a" + "/purchases?key=d7d568706156d07aed570d9ae78d97ab",
+		    success: function(e) {
+		      console.log(e);
+		    }
+	});
+}
